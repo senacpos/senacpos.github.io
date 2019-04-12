1487d4b28133a91e1870391644090d56e85cc514
Matheus Souza <matheusviegasdesouza@gmail.com>
2019-04-12 11:38:50 -0300

    Update modulos.js

--- assets/js/modulos.js
+++ assets/js/modulos.js
@@ -3,27 +3,32 @@ const modulos = [{
         disciplinas: [{
                 id: 'gerencia-configuracao-software',
                 nome: 'Gerência de Configuração de Software',
-                link: 'https://senacpos.github.io/gerencia-configuracao-software'
+                link: 'https://senacpos.github.io/gerencia-configuracao-software',
+                ativo: true
             },
             {
                 id: 'user-experience',
                 nome: 'User Experience',
-                link: 'https://senacpos.github.io/user-experience'
+                link: 'https://senacpos.github.io/user-experience',
+                ativo: true
             },
             {
                 id: 'linguagens-programacao-web',
                 nome: 'Linguagens de Programação para Web',
-                link: 'https://senacpos.github.io/linguagens-programacao-web'
+                link: 'https://senacpos.github.io/linguagens-programacao-web',
+                ativo: true
             },
             {
                 id: 'frameworks-back-end',
                 nome: 'Frameworks de Desenvolvimento Web - Back - end',
-                link: 'https://senacpos.github.io/frameworks-back-end'
+                link: 'https://senacpos.github.io/frameworks-back-end',
+                ativo: true
             },
             {
                 id: 'frameworks-front-end',
                 nome: 'Frameworks de Desenvolvimento Web - Front - end',
-                link: 'https://senacpos.github.io/frameworks-front-end'
+                link: 'https://senacpos.github.io/frameworks-front-end',
+                ativo: true
             }
         ]
     },
@@ -32,27 +37,32 @@ const modulos = [{
         disciplinas: [{
                 id: 'web-semantica',
                 nome: 'Web Semântica',
-                link: 'https://senacpos.github.io/web-semantica'
+                link: 'https://senacpos.github.io/web-semantica',
+                ativo: true
             },
             {
                 id: 'desenvolvimento-aplicacoes-distribuidas',
                 nome: 'Desenvolvimento de Aplicações Web Distribuídas',
-                link: 'https://senacpos.github.io/desenvolvimento-aplicacoes-distribuidas'
+                link: 'https://senacpos.github.io/desenvolvimento-aplicacoes-distribuidas',
+                ativo: true
             },
             {
                 id: 'desenvolvimento-android',
                 nome: 'Desenvolvimento de aplicações para dispositivos móveis Android',
-                link: 'https://senacpos.github.io/desenvolvimento-android'
+                link: 'https://senacpos.github.io/desenvolvimento-android',
+                ativo: false
             },
             {
                 id: 'desenvolvimento-ios',
                 nome: 'Desenvolvimento de aplicações para dispositivos móveis iOS',
-                link: 'https://senacpos.github.io/desenvolvimento-ios'
+                link: 'https://senacpos.github.io/desenvolvimento-ios',
+                ativo: false
             },
             {
                 id: 'tecnologia-bd-dispositivos-moveis',
                 nome: 'Tecnologia de Banco de Dados em Aplicações para dispositivos móveis',
-                link: 'https://senacpos.github.io/tecnologia-bd-dispositivos-moveis'
+                link: 'https://senacpos.github.io/tecnologia-bd-dispositivos-moveis',
+                ativo: false
             }
         ]
     },
@@ -61,27 +71,32 @@ const modulos = [{
         disciplinas: [{
                 id: 'desenvolvimento-dispositivos-moveis-idc',
                 nome: 'Desenvolvimento de aplicações móveis aplicando Internet das Coisas',
-                link: 'https://senacpos.github.io/desenvolvimento-dispositivos-moveis-idc'
+                link: 'https://senacpos.github.io/desenvolvimento-dispositivos-moveis-idc',
+                ativo: false
             },
             {
                 id: 'desenvolvimento-aplicacoes-hibridas',
                 nome: 'Desenvolvimento de aplicações para dispositivos móveis híbridas',
-                link: 'https://senacpos.github.io/desenvolvimento-aplicacoes-hibridas'
+                link: 'https://senacpos.github.io/desenvolvimento-aplicacoes-hibridas',
+                ativo: false
             },
             {
                 id: 'gerenciamento-de-projetos',
                 nome: 'Gerenciamento de Projetos',
-                link: 'https://senacpos.github.io/gerenciamento-de-projetos'
+                link: 'https://senacpos.github.io/gerenciamento-de-projetos',
+                ativo: false
             },
             {
                 id: 'qualidade-de-software',
                 nome: 'Qualidade de Software - Automação de testes',
-                link: 'https://senacpos.github.io/qualidade-de-software'
+                link: 'https://senacpos.github.io/qualidade-de-software',
+                ativo: false
             },
             {
                 id: 'metodologia-e-tcc',
                 nome: 'Metodologia e Desenvolvimento de TCC',
-                link: 'https://senacpos.github.io/metodologia-e-tcc'
+                link: 'https://senacpos.github.io/metodologia-e-tcc',
+                ativo: false
             }
         ]
     }
