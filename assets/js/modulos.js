d46eb7090527efe08f6754851a8df0ec3a0bf378
Matheus Souza <matheusviegasdesouza@gmail.com>
2019-04-12 10:24:08 -0300

    Create modulos.js

---
+++ assets/js/modulos.js
@@ -0,0 +1,88 @@
+const modulos = [{
+        nome: 'Módulo 1',
+        disciplinas: [{
+                id: 'gerencia-configuracao-software',
+                nome: 'Gerência de Configuração de Software',
+                link: 'https://senacpos.github.io/gerencia-configuracao-software'
+            },
+            {
+                id: 'user-experience',
+                nome: 'User Experience',
+                link: 'https://senacpos.github.io/user-experience'
+            },
+            {
+                id: 'linguagens-programacao-web',
+                nome: 'Linguagens de Programação para Web',
+                link: 'https://senacpos.github.io/linguagens-programacao-web'
+            },
+            {
+                id: 'frameworks-back-end',
+                nome: 'Frameworks de Desenvolvimento Web - Back - end',
+                link: 'https://senacpos.github.io/frameworks-back-end'
+            },
+            {
+                id: 'frameworks-front-end',
+                nome: 'Frameworks de Desenvolvimento Web - Front - end',
+                link: 'https://senacpos.github.io/frameworks-front-end'
+            }
+        ]
+    },
+    {
+        nome: 'Módulo 2',
+        disciplinas: [{
+                id: 'web-semantica',
+                nome: 'Web Semântica',
+                link: 'https://senacpos.github.io/web-semantica'
+            },
+            {
+                id: 'desenvolvimento-aplicacoes-distribuidas',
+                nome: 'Desenvolvimento de Aplicações Web Distribuídas',
+                link: 'https://senacpos.github.io/desenvolvimento-aplicacoes-distribuidas'
+            },
+            {
+                id: 'desenvolvimento-android',
+                nome: 'Desenvolvimento de aplicações para dispositivos móveis Android',
+                link: 'https://senacpos.github.io/desenvolvimento-android'
+            },
+            {
+                id: 'desenvolvimento-ios',
+                nome: 'Desenvolvimento de aplicações para dispositivos móveis iOS',
+                link: 'https://senacpos.github.io/desenvolvimento-ios'
+            },
+            {
+                id: 'tecnologia-bd-dispositivos-moveis',
+                nome: 'Tecnologia de Banco de Dados em Aplicações para dispositivos móveis',
+                link: 'https://senacpos.github.io/tecnologia-bd-dispositivos-moveis'
+            }
+        ]
+    },
+    {
+        nome: 'Módulo 3',
+        disciplinas: [{
+                id: 'desenvolvimento-dispositivos-moveis-idc',
+                nome: 'Desenvolvimento de aplicações móveis aplicando Internet das Coisas',
+                link: 'https://senacpos.github.io/desenvolvimento-dispositivos-moveis-idc'
+            },
+            {
+                id: 'desenvolvimento-aplicacoes-hibridas',
+                nome: 'Desenvolvimento de aplicações para dispositivos móveis híbridas',
+                link: 'https://senacpos.github.io/desenvolvimento-aplicacoes-hibridas'
+            },
+            {
+                id: 'gerenciamento-de-projetos',
+                nome: 'Gerenciamento de Projetos',
+                link: 'https://senacpos.github.io/gerenciamento-de-projetos'
+            },
+            {
+                id: 'qualidade-de-software',
+                nome: 'Qualidade de Software - Automação de testes',
+                link: 'https://senacpos.github.io/qualidade-de-software'
+            },
+            {
+                id: 'metodologia-e-tcc',
+                nome: 'Metodologia e Desenvolvimento de TCC',
+                link: 'https://senacpos.github.io/metodologia-e-tcc'
+            }
+        ]
+    }
+];
